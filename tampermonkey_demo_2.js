c69264149a07f183aa40335090dfa22fc779a5e8
xt9852 <xt9852@github.com>
2023-07-01 16:24:21 +0800

    update js

--- tampermonkey_demo_2.js
+++ tampermonkey_demo_2.js
@@ -7,30 +7,44 @@
 // @match        https://v88avnetwork.github.io/88av.html*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @require      https://cdn.jsdelivr.net/npm/hls.js@latest
-// @connect      88av1090.cc
+// @connect      88av1143.cc
+// @grant        GM_getValue
+// @grant        GM_setValue
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 
 var dns;
-var url
+var url;
 var hls;
+var next;
 var last = null;
+
 
 function get_url() {
     console.log("--查找地址开始--");
 
-    url = dns = document.getElementsByTagName('a')[0].href;
+    dns = GM_getValue('domain', '');
+
+    if ('' == dns) {
+        dns = document.getElementsByTagName('a')[0].href;
+        GM_setValue('domain', dns);
+        console.log('set dns:' + dns);
+    }
+
+    url = dns;
 
     let ret;
     let reg = /\?type=(.+?)&page=(\d*)/g;
 
     if ((ret = reg.exec(window.location.search)) !== null) {
         url += ret[1] + "/" + ret[2];
+        next = location.href.substring(0, location.href.lastIndexOf('=') + 1) + (parseInt(ret[2]) + 1);
     }
 
     console.log('dns:' + dns);
     console.log('url:' + url);
+    console.log('next:' + next);
 
     console.log("--查找地址结束--\n\n\n");
 }
@@ -53,7 +67,7 @@ function add_link() {
     document.body = document.createElement("body");
 
     new_link('91', '?type=categories/91&page=1');
-    new_link('ca', '?type=search/ca&page=');
+    new_link('ca', '?type=search/ca&page=1');
     new_link('jav', '?type=jav&page=1');
     new_link('usa', '?type=oumei&page=1');
     new_link('new', '?type=video/latest&page=1');
@@ -100,26 +114,29 @@ function add_page(html) {
         div.innerText = i + ': ' +ret[1];
         document.body.appendChild(div);
 
-        img = document.createElement("video");
-        img.id = "/video/m3u8/" + ret[3] + ".m3u8?video_server=cncdn";
-        img.poster = "https://" + ret[2] + "/videos/" + ret[3] + "/cover/5_505_259";
+        img = document.createElement('video');
+        img.id = '/video/m3u8/' + ret[3] + '.m3u8?video_server=cncdn';
+        img.poster = 'https://' + ret[2] + '/videos/' + ret[3] + '/cover/5_505_259';
         img.onclick = get_m3u8;
-        img.style.cursor = "pointer";
+        img.style = 'cursor:pointer;max-width:100%;max-height:100%';
         document.body.appendChild(img);
 
         console.log(img.id);
     }
 
-    if (1 != i) {
-        reg = /data-total-page=\"([0-9]+)\"/;
-        if ((ret = reg.exec(html)) !== null) { txt = 'total-page=' + ret[1]; }
-    }
+    reg = /data-total-page=\"([0-9]+)\"/;
+    if ((ret = reg.exec(html)) !== null) { txt = 'total-page=' + ret[1]; }
 
     div = document.createElement("div");
     div.innerText = txt;
     document.body.appendChild(div);
 
     console.log(txt);
+
+    div = document.createElement("a");
+    div.href = next;
+    div.innerText = 'next';
+    document.body.appendChild(div);
 
     console.log("--处理页面结束--\n\n\n");
 }
